c12f76a51838db49859e9b16c38a641969f70604
cedricnator <c.kirmayr01@ufromail.cl>
2024-06-12 02:04:43 -0400

    feat(signals): add some stuff

---
+++ src/app/signals/components/side-menu/side-menu.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  title:  string;
+  router: string;
+}
+
+@Component({
+  selector: 'signal-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.css'
+})
+export class SideMenuComponent {
+
+  public menuItems: MenuItem[] = [
+    { title: 'Contador', router: 'counter'},
+    { title: 'Usuario', router: 'user'},
+    { title: 'mutaciones', router: 'properties'}
+  ]
+}
---
+++ src/app/signals/signals-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignalsLayoutComponent } from './layout';
+import { 
+  CounterPageComponent, 
+  UserInfoPageComponent, 
+  PropertiesPageComponent 
+} from './pages';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SignalsLayoutComponent,
+    children: [
+      { 
+        path: 'counter',
+        component: CounterPageComponent
+      },
+      {
+        path: 'user',
+        component: UserInfoPageComponent
+      },
+      {
+        path: 'properties',
+        component: PropertiesPageComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class SignalsRoutingModule { }
