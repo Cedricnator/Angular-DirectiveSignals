98578126fd07ff0d17197d98e121b0ff6e0d86a1
cedricnator <c.kirmayr01@ufromail.cl>
2024-06-12 03:11:48 -0400

    feat(signals): fetch fake data, for testing the signals

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { UserServiceService } from '../../services/user-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-user-info-page',
   templateUrl: './user-info-page.component.html',
   styleUrl: './user-info-page.component.css'
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
+  
+  private userService = inject(UserServiceService);
+  public userId = signal(1);
+  
+  public currentUser = signal<User|undefined>(undefined);
+  public userWasFound = signal(true);
+  
+  loadUser(id: number){
+    if( id <= 0) return;
+    this.userId.set(id);
+
+    this.userService.getUserById(id)
+      .subscribe( user => {
+        this.currentUser.set( user );
+      })
+  }
+
+  ngOnInit(): void {
+    this.loadUser(this.userId())
+  }
 
 }
